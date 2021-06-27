04519b335368fc9d718c40fab01f1b59c075ab0f
Ignacio R. de Almeida <ignacio.rdea@gmail.com>
2021-06-27 12:25:43 -0600

    Everything but big file test works

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -2,7 +2,7 @@ package spell;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class SpellCorrector implements ISpellCorrector{
     private Trie trie = new Trie();
@@ -18,12 +18,87 @@ public class SpellCorrector implements ISpellCorrector{
             trie.add(str);
         }
 
-        System.out.println(trie.toString());
+        //System.out.println(trie.toString());
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) {
         //use set or tree set(java has it)
-        return null;
+        inputWord = inputWord.toLowerCase(Locale.ROOT);
+        TreeSet <String> possibleWords = new TreeSet<>();
+        String similarWord = null;
+        int similarWordCount = 0;
+        int editDistance = 0;
+
+        //If word is in the tree
+        if(trie.find(inputWord) != null){
+            return inputWord;
+        }
+        possibleWords.add(inputWord);
+        do {
+            editDistance++;
+            TreeSet<String> wordVariations = new TreeSet<>();
+            //If word is not found suggest similar
+            for (String it : possibleWords) {
+                //Deletion Distance - Ex: Bird - bir, bid, bdr, ird
+                for (int i = 0; i < it.length(); i++) {
+                    StringBuilder string = new StringBuilder(it);
+                    string.deleteCharAt(i);
+                    wordVariations.add(string.toString());
+                }
+
+                //Transposition Distance - Ex: House - ohuse, huose, hosue, houes
+                for (int i = 0; i < it.length() - 1; i++) {
+                    StringBuilder string = new StringBuilder(it);
+                    char temp = string.charAt(i);
+                    char nextChar;
+                    nextChar = string.charAt(i + 1);
+                    string.setCharAt(i, nextChar);
+                    string.setCharAt(i + 1, temp);
+                    wordVariations.add(string.toString());
+                }
+
+                //Alteration Distance Ex: “top” are “aop”, “bop”, …, “zop”, “tap”, “tbp”, …, “tzp”, “toa”, “tob”, …, and “toz”
+                int ABC_LENGTH = 26;
+                for (int i = 0; i < it.length(); i++) {
+                    StringBuilder string = new StringBuilder(it);
+                    for (int j = 0; j < ABC_LENGTH; j++) {
+                            string.setCharAt(i, (char) ('a' + j));
+                            wordVariations.add(string.toString());
+                    }
+                }
+
+                //Insertion Distance Ex: “ask” are “aask”, “bask”, “cask”, … “zask”, “aask”, “absk”, “acsk”, … “azsk”, “asak”, “asbk”, “asck”, … “aszk”, “aska”, “askb”, “askc”, … “askz”
+                for (int i = 0; i < it.length(); i++) {
+                    for (int j = 0; j < ABC_LENGTH; j++) {
+                        StringBuilder string = new StringBuilder(it);
+                        string.insert(i, (char) ('a' + j));
+                        wordVariations.add(string.toString());
+                    }
+                }
+                for(int i = 0; i < ABC_LENGTH; i++){
+                    wordVariations.add(it + (char)('a' + i));
+                }
+            }
+
+            for (String word : wordVariations) {
+                INode toReturn = trie.find(word);
+                if (toReturn != null) {
+                    if(similarWord == null) {
+                        similarWord = word;
+                        similarWordCount = toReturn.getValue();
+                    }
+                    else {
+                        if (similarWordCount != toReturn.getValue()) {
+                            similarWord = word;
+                            similarWordCount = toReturn.getValue();
+                        }
+                    }
+                }
+            }
+           possibleWords = wordVariations;
+        }while(similarWord == null && editDistance < 2);
+
+        return similarWord;
     }
 }
