6732b37f188e204cc6370ebcdd66652e253cb882
Ignacio R. de Almeida <ignacio.rdea@gmail.com>
2021-06-28 00:04:05 -0600

    Spell Checker Finished

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -1,104 +1,130 @@
 package spell;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class SpellCorrector implements ISpellCorrector{
     private Trie trie = new Trie();
+    private int ABC_LENGTH = 26;
+    private char A = 97;
+
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
         File file = new File(dictionaryFileName);
         Scanner scanner = new Scanner(file);
         scanner.useDelimiter("((#[^\\n]*\\n)|(\\s+))+");
 
-
         while(scanner.hasNext()) {
             String str = scanner.next();
             trie.add(str);
         }
-
         //System.out.println(trie.toString());
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) {
-        //use set or tree set(java has it)
+        TreeSet <String> distanceOneWords = new TreeSet<>();
+        TreeSet <String> distanceTwoWords = new TreeSet<>();
         inputWord = inputWord.toLowerCase(Locale.ROOT);
-        TreeSet <String> possibleWords = new TreeSet<>();
-        String similarWord = null;
-        int similarWordCount = 0;
-        int editDistance = 0;
+        String word = null;
 
         //If word is in the tree
         if(trie.find(inputWord) != null){
             return inputWord;
         }
-        possibleWords.add(inputWord);
-        do {
-            editDistance++;
-            TreeSet<String> wordVariations = new TreeSet<>();
-            //If word is not found suggest similar
-            for (String it : possibleWords) {
+        //If word is not found suggest similar
+        else {
                 //Deletion Distance - Ex: Bird - bir, bid, bdr, ird
-                for (int i = 0; i < it.length(); i++) {
-                    StringBuilder string = new StringBuilder(it);
-                    string.deleteCharAt(i);
-                    wordVariations.add(string.toString());
-                }
+                deletion(inputWord, distanceOneWords);
 
                 //Transposition Distance - Ex: House - ohuse, huose, hosue, houes
-                for (int i = 0; i < it.length() - 1; i++) {
-                    StringBuilder string = new StringBuilder(it);
-                    char temp = string.charAt(i);
-                    char nextChar;
-                    nextChar = string.charAt(i + 1);
-                    string.setCharAt(i, nextChar);
-                    string.setCharAt(i + 1, temp);
-                    wordVariations.add(string.toString());
-                }
+                transposition(inputWord, distanceOneWords);
 
+                //a b c d e f g h i j k l m n o p q r s
                 //Alteration Distance Ex: “top” are “aop”, “bop”, …, “zop”, “tap”, “tbp”, …, “tzp”, “toa”, “tob”, …, and “toz”
-                int ABC_LENGTH = 26;
-                for (int i = 0; i < it.length(); i++) {
-                    StringBuilder string = new StringBuilder(it);
-                    for (int j = 0; j < ABC_LENGTH; j++) {
-                            string.setCharAt(i, (char) ('a' + j));
-                            wordVariations.add(string.toString());
-                    }
-                }
+                //Zupem: i(0): aupem,...,j(18):supem,.., zupem /
+                alteration(inputWord, distanceOneWords);
 
                 //Insertion Distance Ex: “ask” are “aask”, “bask”, “cask”, … “zask”, “aask”, “absk”, “acsk”, … “azsk”, “asak”, “asbk”, “asck”, … “aszk”, “aska”, “askb”, “askc”, … “askz”
-                for (int i = 0; i < it.length(); i++) {
-                    for (int j = 0; j < ABC_LENGTH; j++) {
-                        StringBuilder string = new StringBuilder(it);
-                        string.insert(i, (char) ('a' + j));
-                        wordVariations.add(string.toString());
-                    }
-                }
-                for(int i = 0; i < ABC_LENGTH; i++){
-                    wordVariations.add(it + (char)('a' + i));
-                }
-            }
+                insertion(inputWord, distanceOneWords);
 
-            for (String word : wordVariations) {
-                INode toReturn = trie.find(word);
-                if (toReturn != null) {
-                    if(similarWord == null) {
-                        similarWord = word;
-                        similarWordCount = toReturn.getValue();
-                    }
-                    else {
-                        if (similarWordCount != toReturn.getValue()) {
-                            similarWord = word;
-                            similarWordCount = toReturn.getValue();
-                        }
-                    }
-                }
-            }
-           possibleWords = wordVariations;
-        }while(similarWord == null && editDistance < 2);
+                //check to see if we can findWordFrom Distance 1 set
+                word = findWordFrom(distanceOneWords);
 
-        return similarWord;
+                //check to see if we can findWordFrom Distance 2 set only if we couldn't find the word in Distance 1 set
+            if(word == null){
+                for(String it : distanceOneWords){
+                    deletion(it, distanceTwoWords);
+                    transposition(it, distanceTwoWords);
+                    alteration(it, distanceTwoWords);
+                    insertion(it, distanceTwoWords);
+                }
+                word = findWordFrom(distanceTwoWords);
+            }
+        }
+        return word;
     }
+
+    private String findWordFrom(TreeSet<String> possibleWords){
+        String word = null;
+        int max = 0;
+
+        for(String it : possibleWords) {
+            Node node = (Node) trie.find(it);
+            if (trie.find(it) != null && node.getValue() > max) {
+                max = node.getValue();
+                word = it;
+            }
+        }
+        return word;
+    }
+
+    private void deletion(String inputWord, TreeSet<String> possibleWords) {
+            for (int i = 0; i < inputWord.length(); i++) {
+                StringBuilder string = new StringBuilder(inputWord);
+                string.deleteCharAt(i);
+                possibleWords.add(string.toString());
+            }
+
+    }
+
+    private void transposition(String inputWord, TreeSet<String> possibleWords){
+            for (int i = 0; i < inputWord.length() - 1; i++) {
+                StringBuilder string = new StringBuilder(inputWord);
+                char temp = string.charAt(i);
+                char nextChar;
+                nextChar = string.charAt(i + 1);
+                string.setCharAt(i, nextChar);
+                string.setCharAt(i + 1, temp);
+                possibleWords.add(string.toString());
+            }
+    }
+
+    private void alteration(String inputWord, TreeSet<String> possibleWords){
+            for (int i = 0; i < inputWord.length(); i++) {
+                StringBuilder string = new StringBuilder(inputWord);
+                for (int j = 0; j < ABC_LENGTH; j++) {
+                    string.setCharAt(i, (char) (A + j));
+                    possibleWords.add(string.toString());
+                }
+            }
+
+    }
+
+    private void insertion(String inputWord, TreeSet<String> possibleWords){
+            for (int i = 0; i < inputWord.length(); i++) {
+                for (int j = 0; j < ABC_LENGTH; j++) {
+                    StringBuilder string = new StringBuilder(inputWord);
+                    string.insert(i, (char) (A + j));
+                    possibleWords.add(string.toString());
+                }
+            }
+            for (int i = 0; i < ABC_LENGTH; i++) {
+                possibleWords.add(inputWord + (char) (A + i));
+            }
+        }
+
 }
+
--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -6,7 +6,7 @@ public class Trie implements ITrie{
     private int nodeCount;
     private int wordCount;
     private Node root;
-    int a = 97; //Decimal value of the character 'a'
+    int A = 97; //Decimal value of the character 'a'
 
     public Trie(){
         root = new Node();
@@ -20,14 +20,13 @@ public class Trie implements ITrie{
         INode current = root;
         //Iterate through each character of the word
         for(int i = 0; i < word.length(); i++){
-            int index = word.charAt(i) - a;
+            int index = word.charAt(i) - A;
             INode[] children = current.getChildren();
             //We create the node if it is empty at the Index
             if(children[index] == null) {
                 children[index] = new Node();
                 nodeCount++;
             }
-
             //If node already exists or after it got created, set current so it goes to the next one
             current = current.getChildren()[index];
 
@@ -46,14 +45,12 @@ public class Trie implements ITrie{
 
     @Override
     public INode find(String word) {
-        word.toLowerCase();
         INode current = root;
 
         //Iterate through the trie checking children by children from the beginning
         for(int i = 0; i < word.length(); i++){
-            int index = word.charAt(i) - a;
+            int index = word.charAt(i) - A;
             INode[] children = current.getChildren();;
-
             //Assign the current node to its children if it has a character
             if(children[index] != null){
                 current = children[index];
@@ -63,10 +60,8 @@ public class Trie implements ITrie{
                 break;
             }
             //If it was a char and that happens to be the last of the word, return that word
-            if(i == word.length() - 1) {
-                if (current.getValue() > 0) {
-                    return current;
-                }
+            if(i == word.length() - 1 && current.getValue() > 0) {
+                return current;
             }
         }
         //The word isn't in the trie
@@ -91,7 +86,6 @@ public class Trie implements ITrie{
         //It has a list of all the words found
         StringBuilder words = new StringBuilder();
         toStringTraversal(root, word, words);
-        //toStringTraversal(rootNode, word, words);
         return words.toString();
     }
 
@@ -101,10 +95,7 @@ public class Trie implements ITrie{
         for(int i = 0; i < children.length; i++){
             if(children[i] != null){
                 //The value of 'a' will be 97 so if we add i and convert that to a char it will give us the node letter
-                word.append((char)(i + a));
-
-                //node = node.getChildren()[i];
-
+                word.append((char)(i + A));
                 //check to see if we find the node that has a count greater than 0 meaning it's the end of the word
                 if(children[i].getValue() > 0){
                     words.append(word.toString());
